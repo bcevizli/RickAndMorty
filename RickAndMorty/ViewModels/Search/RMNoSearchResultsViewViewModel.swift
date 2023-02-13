5d0c60ede8dfc852a03476243cfda90831541b52
burak cevizli <burak.cevizli7@gmail.com>
2023-02-13 16:20:47 +0300

    Added No Search Result View

--- RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMNoSearchResultsViewViewModel.swift
@@ -6,7 +6,10 @@
 //
 
 import Foundation
+import UIKit
 
 struct RMNoSearchResultsViewViewModel {
+    let title = "No Results"
+    let image = UIImage(systemName: "magnifyingglass.circle")
     
 }
